41639b069b616a0edf5e1de26b6370e467945e55
Magnus Lind <sungam999@gmail.com>
2019-12-09 17:04:29 +0100

    hmmm

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -7,13 +7,15 @@ Rectangle::Rectangle(Point2D pos, int color, int width, int height)
 
 void Rectangle::render(SDL_Renderer* renderer)
 {
-    printf("RECTANGLE:\n\tpos: %s\n\tcolor: %08X\n\twidht: %d\n\theight: %d\n",
-         position.to_string().c_str(), color, width, height );
+//   printf("RECTANGLE:\n\tpos: %s\n\tcolor: %08X\n\twidht: %d\n\theight: %d\n",
+//        position.to_string().c_str(), color, width, height );
 
     SDL_SetRenderDrawColor(renderer, *(((unsigned char*)&color)+3), *(((unsigned char*)&color)+2), *(((unsigned char*)&color)+1), *(((unsigned char*)&color)+0));
 
-    SDL_RenderDrawLine(renderer, position.x, position.y, position.x, position.y+height);
-    SDL_RenderDrawLine(renderer, position.x, position.y+height, position.x+width, position.y+height);
-    SDL_RenderDrawLine(renderer, position.x, position.y, position.x+width, position.y);
-    SDL_RenderDrawLine(renderer, position.x+width, position.y, position.x+width, position.y+height);
+    int hw = width/2;
+    int hh = height/2;
+    SDL_RenderDrawLine(renderer, position.x-hw, position.y-hh, position.x+hw, position.y-hh );
+    SDL_RenderDrawLine(renderer, position.x+hw, position.y-hh, position.x+hw, position.y+hh );
+    SDL_RenderDrawLine(renderer, position.x+hw, position.y+hh, position.x-hw, position.y+hh );
+    SDL_RenderDrawLine(renderer, position.x-hw, position.y+hh, position.x-hw, position.y-hh );
 }
